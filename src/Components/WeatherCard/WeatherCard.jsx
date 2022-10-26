b39990eef7fdb9cc354c1edf9ade843b125f92f5
Smita Pandya <60938493+smitax21@users.noreply.github.com>
2022-10-26 13:35:40 +0100

    Image code converted to get weather image

--- src/Components/WeatherCard/WeatherCard.jsx
+++ src/Components/WeatherCard/WeatherCard.jsx
@@ -19,9 +19,9 @@ function WeatherCard() {
     windSpeed: "",
   });
 
-  const fetchWeather = async (lat, lon) => {
+  const fetchWeather = async () => {
     let response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=53.38&lon=-1.47&appid=${APIkey}`
+      `https://api.openweathermap.org/data/2.5/forecast?lat=53.38&lon=-1.47&appid=${APIkey}`
     );
     let jsonResponse = await response.json();
     console.log(jsonResponse);
@@ -43,26 +43,29 @@ function WeatherCard() {
       "Dec",
     ];
 
-    let unixTimestamp = jsonResponse.dt;
+    let unixTimestamp = jsonResponse.list[0].dt;
     let jsdate = new Date(unixTimestamp * 1000);
     const nameDay = days[jsdate.getDay(jsdate)];
     const month = months[jsdate.getMonth(jsdate)];
     const date = jsdate.getDate(jsdate);
 
     // convert kelvin to celcius
-    let kelvinMax = jsonResponse.main.temp_max;
+    let kelvinMax = jsonResponse.list[0].main.temp_max;
     let maxTemp = Math.floor(kelvinMax - 273.15);
-    let kelvinMin = jsonResponse.main.temp_min;
+    let kelvinMin = jsonResponse.list[0].main.temp_min;
     let minTemp = Math.floor(kelvinMin - 273.15);
 
+    //convert icon code to image
+    let iconcode = jsonResponse.list[0].weather[0].icon;
+    let iconURL = "http://openweathermap.org/img/w/" + iconcode + ".png";
+
     createCards({
-      date: date + " " + month,
-      dayOfWeek: nameDay,
-      picture: jsonResponse.weather[0].icon,
-      description: jsonResponse.weather[0].description,
+      date: date + "-" + month + "-" + nameDay,
+      picture: iconURL,
+      description: jsonResponse.list[0].weather[0].description,
       tempMax: maxTemp,
       tempMin: minTemp,
-      windSpeed: jsonResponse.wind.speed,
+      windSpeed: jsonResponse.list[0].wind.speed,
     });
   };
 
@@ -72,16 +75,16 @@ function WeatherCard() {
 
   return (
     <Card style={{ width: "300px" }}>
-      <Card.Header>
-        {weatherCards.date + ", "} {weatherCards.dayOfWeek}
-      </Card.Header>
+      <Card.Header>{weatherCards.date}</Card.Header>
 
       <Card.Body>
         <Card.Img
-          className="cardImage"
+          id="wicon"
+          className="cardImage mx-auto"
           variant="top"
           src={weatherCards.picture}
         />
+
         <Card.Title className="title">{weatherCards.description}</Card.Title>
 
         <Row>
